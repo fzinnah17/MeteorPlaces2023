1776248b112c17fcc1692f367db916118308d5d5
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-05-15 11:46:37 -0400

    added new layout for landing

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,10 +1,44 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// const Landing = () => {
+//   return (
+//     <div className="landing-container">
+//       <div className="hero-container">
+//         <p className="hero-hook">
+//           Discover new experiences and make memories with friends and family
+//         </p>
+//         <h1 className="hero-title">
+//           Find nearby places based on your zip code
+//         </h1>
+//         <p className="hero-text">
+//           Discover new places to eat, drink, shop, and play with our easy-to-use app
+//         </p>
+//         <div className="hero-cta">
+//           <Link to="/register" className="btn btn-primary btn-wave">
+//             Register
+//           </Link>
+//           <Link to="/login" className="btn btn-secondary btn-wave">
+//             Log In
+//           </Link>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Landing;
+
+
 import React from "react";
 import { Link } from "react-router-dom";
+import videoFile from "../videos/landingvideo.mp4"; 
+
 
 const Landing = () => {
   return (
     <div className="landing-container">
-      <div className="hero-container">
+      <div className="left-section">
         <p className="hero-hook">
           Discover new experiences and make memories with friends and family
         </p>
@@ -22,6 +56,9 @@ const Landing = () => {
             Log In
           </Link>
         </div>
+      </div>
+      <div className="right-section">
+      <video src={videoFile} controls autoplay muted />
       </div>
     </div>
   );
