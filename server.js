a009f68e395d2b02a694a0c979d98efb8e2cdf56
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-05-18 11:48:50 -0400

    changes to mongodb connection

--- server.js
+++ server.js
@@ -76,9 +76,13 @@ const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
+
 
 // Passport middleware
 app.use(passport.initialize());
